324300ffaf594f0b4603078d230cbdc9028de27a
James Tiu <jamestiulawyer@gmail.com>
2021-04-23 17:28:45 -0600

    CreatedClasses

---
+++ TestLib.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzTest
+{
+    [TestFixture]
+    public class FizzBuzzerTest
+    {
+        
+       [Test]
+       public void SampleTest()
+        {
+           [Test]
+           public void Buzzer_When1_Returns1()
+            {
+                //arrange
+                int input = 1;
+
+                //act
+
+                //assert
+            }
+          
+        }
+    }
+}
